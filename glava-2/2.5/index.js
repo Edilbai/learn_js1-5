49373f006d4680ea3b9dc8c87bd378bbf97a35b2
Edilbai <thisisspar49@gmail.com>
2023-03-01 21:02:20 +0600

    first commit

---
+++ glava-2/2.5/index.js
@@ -0,0 +1,20 @@
+// Что выведет этот скрипт?
+
+// let name = 'Ilya'
+
+// alert(`hello ${1}`) // ?
+
+// alert(`hello ${'name'}`) // ?
+
+// alert(`hello ${name}`) // ?
+
+let name = 'Ilya'
+
+// выражение - число 1
+alert(`hello ${1}`) // hello 1
+
+// выражение - строка "name"
+alert(`hello ${'name'}`) // hello name
+
+// выражение - переменная, вставим её в строку
+alert(`hello ${name}`) // hello Ilya
---
+++ glava-2/2.9/index.js
@@ -0,0 +1,18 @@
+// Каким будет результат этих выражений?
+
+// 5 > 4
+// "ананас" > "яблоко"
+// "2" > "12"
+// undefined == null
+// undefined === null
+// null == "\n0\n"
+// null === +"\n0\n"
+
+// ответ:
+// true
+// false
+// true
+// true
+// false
+// false
+// false
